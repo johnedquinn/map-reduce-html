dde26861413432c7a0c95249d6b121aacb3643fd
johnedquinn <johnedquinn@gmail.com>
2021-03-24 11:27:55 -0400

    Add Bigrams functionality

--- run.py
+++ run.py
@@ -21,19 +21,35 @@ def HtmlToWords():
 def WordCount():
 
 	WORDS_DIR   = '/users/jquinn13/Words'
-	WC_DIR = '/users/jquinn13/WordCount'
+	OUT_DIR = '/users/jquinn13/WordCount'
 
 	print('Running WordCount Map-Reduce')
 	command = f'''
 		hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 		-files    WordCountMap.py,WordCountReduce.py \
 		-input    {WORDS_DIR} \
-		-output   {WC_DIR} \
+		-output   {OUT_DIR} \
 		-mapper   WordCountMap.py \
 		-reducer  WordCountReduce.py
+	'''
+	os.system(command)
+
+def Bigrams():
+
+	WORDS_DIR   = '/users/jquinn13/Words'
+	OUT_DIR = '/users/jquinn13/Bigrams'
+
+	print('Running WordCount Map-Reduce')
+	command = f'''
+		hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+		-files    BigramsMap.py,BigramsReduce.py \
+		-input    {WORDS_DIR} \
+		-output   {OUT_DIR} \
+		-mapper   BigramsMap.py \
+		-reducer  BigramsReduce.py
 	'''
 	os.system(command)
 	
 if __name__ == '__main__':
 
-	WordCount()
+	Bigrams()
