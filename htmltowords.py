14f5c61faba371cafbb546e3f51e9db64f754e7c
johnedquinn <johnedquinn@gmail.com>
2021-03-24 21:14:21 -0400

    Add option for host:words

--- htmltowords.py
+++ htmltowords.py
@@ -12,11 +12,19 @@ import re
 import urllib.parse
 import string
 import io
+import os
 
 
 ### Main Execution
 
 if __name__ == '__main__':
+
+	INCLUDE_HOST = False
+
+	# Parse Command Line
+	if len(sys.argv) == 2:
+		if sys.argv[1] == '-h':
+			INCLUDE_HOST = True
 
 	# Add UTF-8 Support
 	stream = io.TextIOWrapper(sys.stdin.buffer, encoding='iso-8859-1')
@@ -47,6 +55,11 @@ if __name__ == '__main__':
 				if word.isnumeric() or not word.isalpha(): continue
 				if len(word) < 3: continue
 				
-				print(word.lower())
+				# Print
+				if not INCLUDE_HOST:
+					print(word.lower())
+				else:
+					in_file = os.getenv('mapreduce_map_input_file')
+					print(f'{in_file}\t{word.lower()}')
 
 	sys.exit(0)
